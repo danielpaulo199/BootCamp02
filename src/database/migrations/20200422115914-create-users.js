bd6ec0ad00d86cc912895ab48e8e43f4c5d368f4
Daniel Paulo Dos Santos <daniel223618@br-atacadao.corp>
2020-04-24 10:42:33 -0300

    refactor: Mudança na configuração do banco para acessar databse ( gobarber(daniel) ). NO MIGRATIONS YET

--- src/database/migrations/20200422115914-create-users.js
+++ src/database/migrations/20200422115914-create-users.js
@@ -7,14 +7,35 @@ module.exports = {
         autoIncrement: true,
         primaryKey: true,
       },
+
       name: {
         type: Sequelize.STRING,
         alowNull: false,
       },
+
       email: {
         type: Sequelize.STRING,
         alowNull: false,
         unique: true,
+      },
+      password_hash: {
+        type: Sequelize.STRING,
+        alowNull: false,
+      },
+      provider: {
+        type: Sequelize.BOOLEAN,
+        default: false,
+        alowNull: false,
+      },
+
+      created_at: {
+        type: Sequelize.DATE,
+        alowNull: false,
+      },
+
+      updated_at: {
+        type: Sequelize.DATE,
+        alowNull: false,
       },
     });
   },
