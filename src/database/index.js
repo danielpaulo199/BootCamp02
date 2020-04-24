1b8bde31277196ba67f9a1a034d80543738ec7f7
Daniel Paulo Dos Santos <daniel223618@br-atacadao.corp>
2020-04-24 13:48:19 -0300

    aula 10: criando loader models

---
+++ src/database/index.js
@@ -0,0 +1,19 @@
+import Sequelize from 'sequelize';
+import databaseConfig from '../config/database';
+
+import User from '../app/models/Users'; // importar models
+
+const models = [User]; // Array que armaena todos os models
+
+class Database {
+  constructor() {
+    this.init();
+  }
+
+  init() {
+    this.connection = new Sequelize(databaseConfig); // parametros de conecxão
+    models.map((model) => model.init(this.connection));
+  }
+}
+
+export default new Database();
