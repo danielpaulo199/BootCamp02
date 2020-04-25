079d91bb28b19c5ff827b664e9e2e12708da717c
Daniel Paulo <Danielpaulo199@gmail.com>
2020-04-25 08:19:47 -0300

    Working on models loader

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,21 @@
+import User from './app/models/Users';
+
 const { Router } = require('express');
 
 const routes = new Router();
 
-routes.get('/', (req, res) => res.json({ message: 'bala' }));
+routes.get('/', async (req, res) => {
+  try {
+    const user = await User.create({
+      name: 'Diego',
+      email: 'diego@rock.com.br',
+      password_hash: '12345678',
+    });
+
+    return res.json(user);
+  } catch (error) {
+    return res.send(error);
+  }
+});
 
 export default routes;
