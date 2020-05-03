32dea53942d84188ce020a3b218287d8a209ee00
Daniel Paulo Dos Santos <daniel223618@br-atacadao.corp>
2020-05-03 09:18:11 -0300

    Usser controle refactor

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -30,7 +30,19 @@ class UserController {
       name: Yup.string(),
       email: Yup.string().email(),
       oldPassword: Yup.string().min(6),
+      password: Yup.string()
+        .min(6)
+        .when('oldPassword', (oldPassword, field) =>
+          oldPassword ? field.required() : field
+        ),
+      confirmPassword: Yup.string().when('password', (password, field) =>
+        password ? field.required().oneOf([Yup.ref('password')]) : field
+      ),
     });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(401).json({ error: 'Validation Fails' });
+    }
 
     const { email, oldPassword } = req.body;
 
