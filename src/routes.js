d205325b6e9793feb2bf3c8728b62ae000398438
Daniel Paulo Dos Santos <daniel223618@br-atacadao.corp>
2020-05-03 13:19:52 -0300

    Add: Avatar_id relations database

--- src/routes.js
+++ src/routes.js
@@ -4,7 +4,7 @@ import multerConfig from './config/multer';
 import SessionController from './app/controllers/SessionController';
 import UserController from './app/controllers/UserController';
 import FileController from './app/controllers/FileController';
-
+import ProviderController from './app/controllers/ProviderController';
 import authMiddleware from './app/middlewares/auth';
 
 const upload = multer(multerConfig);
@@ -12,6 +12,7 @@ const routes = new Router();
 
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
+routes.get('/providers', ProviderController.index);
 
 routes.use(authMiddleware);
 
