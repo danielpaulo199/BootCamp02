557e9526613265e15d479ddb7d247ef0c20c2e98
Daniel Paulo <danielpaulo199@gmail.com>
2020-05-05 00:46:52 -0300

    Add: Schedule Controller and index

--- src/routes.js
+++ src/routes.js
@@ -7,6 +7,7 @@ import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
 import authMiddleware from './app/middlewares/auth';
 import AppointmentController from './app/controllers/AppointmentController';
+import ScheduleController from './app/controllers/ScheduleController';
 
 const upload = multer(multerConfig);
 const routes = new Router();
@@ -20,6 +21,7 @@ routes.use(authMiddleware);
 routes.put('/users', UserController.update);
 routes.post('/appointments', AppointmentController.store);
 routes.get('/appointments', AppointmentController.index);
+routes.get('/schedule', ScheduleController.index);
 routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
