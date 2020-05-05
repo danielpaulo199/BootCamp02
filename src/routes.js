bef3975542a3f251a86a0c1e07834440d4de4777
Daniel Paulo <danielpaulo199@gmail.com>
2020-05-05 15:37:57 -0300

    Add: Read Notifications

--- src/routes.js
+++ src/routes.js
@@ -25,5 +25,6 @@ routes.get('/appointments', AppointmentController.index);
 routes.get('/schedule', ScheduleController.index);
 routes.post('/files', upload.single('file'), FileController.store);
 routes.get('/notifications', NotificationController.index);
+routes.put('/notifications/:id', NotificationController.update);
 
 export default routes;
